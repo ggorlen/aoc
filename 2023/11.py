3a6d50af9a3e0bc16059ae18289042dcee924cc8
ggorlen <gsgorlen@gmail.com>
2024-02-08 13:07:48 -0800

    Add 2023 day 11 part 2

--- 2023/11.py
+++ 2023/11.py
@@ -1,36 +1,36 @@
-def rotate(m):
-    return list(zip(*m[::-1]))
-
-def expand(grid):
-    bigger = []
-
-    for i, x in enumerate(grid):
-        if "." * len(x) == "".join(x):
-            bigger.append(x[:])
-
-        bigger.append(x[:])
-
-    return bigger
-
-
-def dist(a, b):
+def manhattan_dist(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-with open("11.txt") as f:
-    grid = [list(x.strip()) for x in f.readlines()]
 
-grid = rotate(rotate(rotate(expand(rotate(expand(grid))))))
-locations = []
+def sum_all_pairs_distances(grid, gap):
+    locations = []
+    vertical_boost = 0
 
-for y, row in enumerate(grid):
-    for x, cell in enumerate(row):
-        if cell == "#":
-            locations.append((y, x))
+    for y, row in enumerate(grid):
+        horizontal_boost = 0
 
-total_dists = 0
+        if all(c == "." for c in row):
+            vertical_boost += gap
 
-for i, a in enumerate(locations):
-    for b in locations[i+1:]:
-        total_dists += dist(a, b)
+        for x, cell in enumerate(row):
+            if all(grid[i][x] == "." for i in range(len(grid[x]))):
+                horizontal_boost += gap
 
-print(total_dists)
+            if cell == "#":
+                locations.append((y + vertical_boost, x + horizontal_boost))
+
+    total_dists = 0
+
+    for i, a in enumerate(locations):
+        for b in locations[i+1:]:
+            total_dists += manhattan_dist(a, b)
+
+    return total_dists
+
+
+if __name__ == "__main__":
+    with open("11.txt") as f:
+        grid = [x.strip() for x in f.readlines()]
+
+    print(sum_all_pairs_distances(grid, gap=1))
+    print(sum_all_pairs_distances(grid, gap=999_999))
