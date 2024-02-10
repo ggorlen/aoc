6d3a6da7420705254cb199a55a8e7cb0b06b0f56
ggorlen <gsgorlen@gmail.com>
2024-02-10 10:28:50 -0800

    Add 2023.9.2

--- 2023/9.py
+++ 2023/9.py
@@ -1,11 +1,31 @@
+# part 1
 with open("9.txt") as f:
-    total = 0
+    lines = [list(map(int, x.split())) for x in f.readlines()]
 
-    for line in f.readlines():
-        line = list(map(int, line.split()))
+total = 0
 
-        while any(x != 0 for x in line):
-            total += line[-1]
-            line = [line[i+1] - line[i] for i in range(len(line) - 1)]
+for line in lines:
+    while any(line):
+        total += line[-1]
+        line = [line[i+1] - line[i] for i in range(len(line) - 1)]
 
-    print(total)
+print(total)
+
+# part 2
+total = 0
+
+for line in lines:
+    firsts = []
+
+    while any(line):
+        firsts.append(line[0])
+        line = [line[i+1] - line[i] for i in range(len(line) - 1)]
+
+    curr = 0
+
+    for x in reversed(firsts):
+        curr = x - curr
+
+    total += curr
+
+print(total)
